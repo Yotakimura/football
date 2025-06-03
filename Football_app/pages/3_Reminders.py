36c30f25512f5ef307a3065dfe2826e864e71078
Yotakimura <yotakimu63@gmail.com>
2025-06-03 11:47:40 -0400

    Update 3_Reminders.py

--- Football_app/pages/3_Reminders.py
+++ Football_app/pages/3_Reminders.py
@@ -1,38 +1,3 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "43c846d7-0fa8-424c-a05f-05aa9162c376",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python [conda env:base] *",
-   "language": "python",
-   "name": "conda-base-py"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.12.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
-
-
 import streamlit as st
 import smtplib
 from email.message import EmailMessage
