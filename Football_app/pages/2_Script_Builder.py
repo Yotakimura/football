1cb3cae834364384868a7a103b89a4df93e22ffa
Yotakimura <yotakimu63@gmail.com>
2025-06-03 11:48:19 -0400

    Update and rename Script_Builder.py to 2_Script_Builder.py

--- Football_app/pages/2_Script_Builder.py
+++ Football_app/pages/2_Script_Builder.py
@@ -1,38 +1,3 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "372476e4-feb8-43fe-9d03-12ce0b143faf",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python [conda env:base] *",
-   "language": "python",
-   "name": "conda-base-py"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.12.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
-
-
 import streamlit as st
 import pandas as pd
 import json
